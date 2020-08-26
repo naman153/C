5cc08c05c82674fcbc24eaa69436de9c0efd4890
Naman Gupta <67692721+naman153@users.noreply.github.com>
2020-08-26 13:34:46 +0530

    add

---
+++ Drawing_book.c
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int pageCount(int n, int p) {
+    int min=0;
+    min=(n/2)-(p/2);
+    if(min>p/2){
+        min=p/2;
+    }
+    
+    return min;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int p;
+    cin >> p;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = pageCount(n, p);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
